00a2af065223f907d29e812408e2d8e14e6fa47f
HelloAIJ <102969341+HelloAIJ@users.noreply.github.com>
2024-02-16 16:49:37 +0900

    Add files via upload
    common 추가

--- Book/Doit_JSP/06/js/keycode.js
+++ Book/Doit_JSP/06/js/keycode.js
@@ -2,8 +2,5 @@ const body = document.body;
 const result = document.querySelector("#result");
 
 body.addEventListener("keydown", (e) => {
-  result.innerText = `
-  code : ${e.code}, 
-  key : ${e.key}
-  `;
+  result.innerText = `code : ${e.code} / key : ${e.key}`;
 });
